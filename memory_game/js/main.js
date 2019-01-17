44f62f40656cc2752369efe850048ea0013fe287
SANG CHOI <sangchoi@SANGs-MacBook-Pro.local>
2019-01-17 09:56:20 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,8 +23,17 @@ var cards = [
 }
 ];
 
-var cardsInPlay = [];
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
 
+var cardsInPlay = [];
 var checkForMatch = function() {
 if (cardsInPlay[0] === cardsInPlay[2]) {
 	alert("You found a match!");
@@ -35,23 +44,19 @@ if (cardsInPlay[0] === cardsInPlay[2]) {
 }
 
 
-
-var flipCard = function(cardId) {
+var flipCard = function() {
+var cardId = this.getAttribute("data-id");
 console.log("User flipped" + " " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);{
+cardsInPlay.push(cards[cardId].rank);
+this.setAttribute("src", cards[cardId].cardImage);
 if (cardsInPlay.length === 2) 
-	
-
-checkForMatch(); 
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
-
+checkForMatch(); 
 };
-}
 
-flipCard(0);
 
-flipCard(2);
+createBoard();
 
 
 
